c5f3dd6a3c7d20174b6624c4106c71a30c42e508
amphineko <sukareki@gmail.com>
2018-05-26 11:29:23 +0800

    handler.null: initial commit

---
+++ Akizuki.Handler.Null/NullHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using moe.futa.akizuki.Core.Extensions.Handlers;
+using moe.futa.akizuki.Core.Messages;
+using NLog;
+
+namespace Akizuki.Handler.Null
+{
+    public class NullHandler : AbstractHandler
+    {
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public override async Task<bool> AcceptAsync(AbstractStatus status)
+        {
+            // return true to indicate handled
+            // statuses won't be passed to next handlers
+            // TODO: prevent duplicated dump with consoledump handler
+            _logger.Debug($"INBOUND: {status}");
+            return await Task.FromResult(true);
+        }
+
+        public override void SetDisabled()
+        {
+            base.SetDisabled();
+            _logger.Info("NullHandler has been disabled");
+        }
+
+        public override void SetEnabled()
+        {
+            base.SetEnabled();
+            _logger.Warn("NullHandler has been enabled, next handlers won't receive any statuses");
+        }
+    }
+}
