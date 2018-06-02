d0f2e890bb7434ac8e4cce146ba9e5f011e58abb
amphineko <sukareki@gmail.com>
2018-06-02 14:00:50 +0800

    bootloader: disable extensions and shutdown

--- Akizuki.Bootloader/Bootloader.cs
+++ Akizuki.Bootloader/Bootloader.cs
@@ -13,37 +13,75 @@ namespace Akizuki.Bootloader
 {
     internal class Bootloader
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        ///     Interlocked variable for discarding multiple shutdown events
+        /// </summary>
+        private static int _exited = 0;
+
         private static void Main(string[] args)
         {
-            var logger = LogManager.GetCurrentClassLogger();
-            logger.Info("Akizuki bootstrap started");
+            Logger.Info("Akizuki bootstrap started");
 
+            // load extensions from assembly files in ./Extensions/
             var classRepo = new ClassRepository();
-            logger.Info($"Loading extensions");
+            Logger.Info($"Loading extensions");
             classRepo.LoadDirectory(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ??
                                                  throw new InvalidOperationException(), "Extensions"));
 
+            // load and parse configuration XML document
             var configPath = Path.ChangeExtension(Assembly.GetEntryAssembly().Location, "xml");
-            logger.Info($"Loading XML {configPath}");
+            Logger.Info($"Loading XML {configPath}");
             var config = LoadConfigurationFile(configPath, classRepo.GetConfigurationTypes());
 
+            // create routers
             var inRouter = new InboundRouter();
             var outRouter = new OutboundRouter();
 
-            foreach (var instance in config.Extensions.Install(classRepo, inRouter, outRouter))
+            // create extension instances
+            var instances = config.Extensions.Install(classRepo, inRouter, outRouter);
+            foreach (var instance in instances)
                 instance.SetEnabled();
 
-            logger.Info("Akizuki bootstrap completed");
+            // register shutdown events
+            // TODO: complete missing shutdown events
+            AppDomain.CurrentDomain.ProcessExit += (sender, eventArgs) => Shutdown(instances);
+            Console.CancelKeyPress += (sender, eventArgs) => Shutdown(instances);
+
+            // sleep the bootloader thread when bootstrap jobs are done
+            Logger.Info("Akizuki bootstrap completed");
             Thread.Sleep(Timeout.Infinite);
         }
 
-        public static Configuration LoadConfigurationFile(string path, List<Type> extraTypes)
+        private static Configuration LoadConfigurationFile(string path, List<Type> extraTypes)
         {
+            // extraTypes are used to parse Extensions' configuration classes
             var serializer = new XmlSerializer(typeof(Configuration), extraTypes.ToArray());
             using (var stream = new FileStream(path, FileMode.Open))
-            {
                 return (Configuration) serializer.Deserialize(stream);
+        }
+
+        private static void Shutdown(IList<AbstractExtension> instances)
+        {
+            // multiple shutdown processes are not allowed
+            if (Interlocked.CompareExchange(ref _exited, 1, 0) == 1)
+            {
+                Logger.Error("Triggered multiple shutdown events");
+                return;
             }
+            
+            Logger.Info("Akizuki bootstrap stopping");
+
+            // disable active extension instances
+            foreach (var instance in instances)
+                instance.SetDisabled();
+
+            // disabled extensions may have resources to dispose
+            foreach (var instance in instances)
+                instance.Dispose();
+
+            Logger.Info("Akizuki bootstrap stopped");
         }
     }
 }
